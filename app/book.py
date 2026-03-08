c92d86af486b0200485b526534a082e573ca6281
Yurii Palahitskyi <brebston7@gmail.com>
2026-03-08 17:03:06 +0100

    Fixed issues #2

--- app/book.py
+++ app/book.py
@@ -1,33 +1,4 @@
-from app.strategies.display import display_strategies
-from app.strategies.print import print_strategies
-from app.strategies.serialize import serializers
-
-
 class Book:
     def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
-
-    def display(self, display_type: str) -> None:
-        strategy = display_strategies.get(display_type)
-
-        if not strategy:
-            raise ValueError(f"Unknown display type: {display_type}")
-
-        strategy.display(self)
-
-    def print_book(self, print_type: str) -> None:
-        strategy = print_strategies.get(print_type)
-
-        if not strategy:
-            raise ValueError(f"Unknown print type: {print_type}")
-
-        strategy.print_book(self)
-
-    def serialize(self, serialize_type: str) -> str:
-        serializer = serializers.get(serialize_type)
-
-        if not serializer:
-            raise ValueError(f"Unknown serialize type: {serialize_type}")
-
-        return serializer.serialize(self)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 from app.book import Book
+from app.strategies.display import display_strategies
+from app.strategies.print import print_strategies
+from app.strategies.serialize import serializers
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd == "display":
-            book.display(method_type)
+            strategy = display_strategies.get(method_type)
+            strategy.display(book)
         elif cmd == "print":
-            book.print_book(method_type)
+            strategy = print_strategies.get(method_type)
+            strategy.print_book(book)
         elif cmd == "serialize":
-            return book.serialize(method_type)
+            serializer = serializers.get(method_type)
+            return serializer.serialize(book)
 
 
 if __name__ == "__main__":
